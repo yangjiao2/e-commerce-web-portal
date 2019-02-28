a7c1e6dcaae87a924d8a84f05941fb71d00dec7f
Csy817 <1075208266@qq.com>
2019-02-28 14:53:31 +0800

    修改

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 
 import All from './all'
+import CartOrder from './order'
 import './index.css'
 
 const Cart = () => (
@@ -9,6 +10,7 @@ const Cart = () => (
         <Switch>
             <Route exact path="/cart" component={All}/>
             <Route exact path="/cart/all" component={All}/>
+            <Route path="/cart/order" component={CartOrder}/>
         </Switch>
     </div>
 )
