541baee1d54286a054ae5c8a4be2ba9bddbbc0f0
Csy817 <1075208266@qq.com>
2019-03-04 14:18:15 +0800

    shop cart delete goods OK in edit page

--- src/configs/url.js
+++ src/configs/url.js
@@ -1,4 +1,5 @@
-// const graphqlFC = 'https://ec.ioobot.cn/graphql';
-const graphqlFC = 'http://ecenhance.ioobot.cn/graphql';
+// const graphqlFC = 'https://ec.ioobot.cn/graphql';            // old schema fc mongodb
+// const graphqlFC = 'http://ecenhance.ioobot.cn/graphql';      // new schema fc mongodb
+const graphqlFC = 'http://ecommerce.ioobot.cn/graphql';      // new schema server mongodb
 
 export {graphqlFC}
